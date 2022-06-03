5f85c4448f0b7f23e5a17cef73c70e5224ca5e55
Bekzat Bakirov <bekzat.bakirov.alt@gmail.com>
2022-06-03 12:46:37 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// 1.
+// let myNumber = function (howManyTimes) {
+
+// 	for (let i = 0; i < howManyTimes; i++) {
+// 	console.log(i)
+// 	}
+// }
+// myNumber(50)
+	
+
+
+
+
+
+// 2.
+// let myNum = function (howManyTimes) {
+
+// 	for (let i = 50; i > howManyTimes; i--) {
+// 	console.log(i)
+// 	}
+// }
+// myNum(0)
+
+
+
+
+3
+let myArray = [2, 5, 9, 15, 0, 4];
+
+for (let i = 0; i < myArray.length; i++ ) {
+
+	if(myArray[i] > 2 && myArray[i] < 5) 
+    console.log(myArray[i])
+}
